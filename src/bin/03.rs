ca6a4ccf1c0f91c527045db0dd911f2d09deef7a
Fadil Nohur <fadil.nohur@outlook.com>
2023-04-01 23:06:00 +0100

    Day 3 - update to use iterators

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -3,61 +3,70 @@ use itertools::Itertools;
 
 const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// fn compartment_to_set
+fn rucksack_items_to_set(items: &str) -> HashSet<char> {
+    HashSet::from_iter(items.chars())
+}
+
+fn common_item(group: impl Iterator<Item = HashSet<char>> ) -> char {
+    let common_items: HashSet<char> = group.reduce(
+        |set1, set2| set1.intersection(&set2).copied().collect()
+    ).unwrap();
+
+    assert_eq!(common_items.len(), 1);
+
+    let common_item = *common_items.iter().collect::<Vec<&char>>()[0];
+
+    common_item
+}
+
+fn sum_priorities(priorities: impl Iterator<Item = char>) -> u32 {
+    let alphabet_vec: Vec<char> = ALPHABET.chars().collect();
+    let sum_priorities: usize = priorities
+        .map(
+            |item| 
+            alphabet_vec
+                .iter()
+                .position(
+                    |&char| char == item
+                )
+                .unwrap() 
+                + 1
+        )
+        .sum();
+    let sum_priorities = u32::try_from(sum_priorities).unwrap();
+    sum_priorities
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let compartment_to_set = |compartment: &str| -> HashSet<char> {
-        HashSet::from_iter(compartment.chars())
-    };
-
     let rucksacks = input
         .lines()
         .map(|rucksack| {
             let compartments = rucksack.split_at(rucksack.len() / 2);
 
-            let left_set= compartment_to_set(compartments.0);
-            let right_set = compartment_to_set(compartments.1);
-
-            let inter: Vec<&char> = left_set.intersection(&right_set).collect();
-
-            let result = inter[0];
-
-            *result
+            common_item([
+                rucksack_items_to_set(compartments.0), 
+                rucksack_items_to_set(compartments.1)
+            ].into_iter())
         });
-    let alphabet_vec: Vec<char> = ALPHABET.chars().collect();
-    let sum_priorities: usize = rucksacks.map(
-        |item| 
-        alphabet_vec.iter().position(|&cha| cha == item).unwrap() + 1
-    ).sum();
-    let sum_priorities = u32::try_from(sum_priorities).unwrap();
+    let sum_priorities = sum_priorities(rucksacks);
     Some(sum_priorities)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut rucksack_groups: Vec<char> = Vec::new();
-    let groups = input.lines().chunks(3);
-    for chunk in &groups {
+    let rucksack_groups = input
+        .lines()
+        .map(
+            |rucksack| rucksack_items_to_set(rucksack)
+        )
+        .chunks(3);
 
-        let chunk = chunk.map(|inv| HashSet::from_iter(inv.chars()));
-
-        let common: HashSet<char> = chunk.reduce(
-            |set1, set2| set1.intersection(&set2).copied().collect()
-        ).unwrap();
-
-        assert_eq!(common.len(), 1);
-
-        let common = *common.iter().collect::<Vec<&char>>()[0];
-
-        rucksack_groups.push(common);
-    }
-    let alphabet_vec: Vec<char> = ALPHABET.chars().collect();
     let rucksack_groups = rucksack_groups.into_iter();
 
-    let sum_priorities: usize = rucksack_groups.map(
-        |item| 
-        alphabet_vec.iter().position(|&cha| cha == item).unwrap() + 1
-    ).sum();
-    let sum_priorities = u32::try_from(sum_priorities).unwrap();
+    let common_items = rucksack_groups.map(
+        |group| common_item(group)
+    );
+
+    let sum_priorities = sum_priorities(common_items);
     Some(sum_priorities)
 }
 
