6d4a817cd28139090d9369da13b231ea45a01855
Fadil Nohur <fadil.nohur@outlook.com>
2023-04-07 21:19:12 +0100

    Day 2 Part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -13,7 +13,7 @@ enum Outcome {
 }
 
 impl Shape {
-    fn from_self(shape: char) -> Self {
+    fn from_opponent(shape: char) -> Self {
         match shape {
             'A' => Shape::ROCK,
             'B' => Shape::PAPER,
@@ -22,7 +22,7 @@ impl Shape {
         }
     }
 
-    fn from_opponent(shape: char) -> Self {
+    fn from_self(shape: char) -> Self {
         match shape {
             'X' => Shape::ROCK,
             'Y' => Shape::PAPER,
@@ -42,37 +42,34 @@ impl Outcome {
         }
     }
 }
-enum RoundType {
-    SELF,
-    ELF,
-}
 
 struct SelfRound {
+    opponent: Shape,    
     self_: Shape,
-    opponent: Shape,
 }
 
 struct ElfRound {
-    self_: Shape,
+    opponent: Shape,
     outcome: Outcome,
 }
 
-trait Round {
-    fn from_str(round: &str) -> Self;
+
+fn get_char(str: &str) -> char {
+    str.chars().next().unwrap()
 }
 
-impl Round for SelfRound {
-    fn from_str(round: &str) -> Self {
-        let get_char = |str: &str| str.chars().next().unwrap();
 
+impl SelfRound {
+    fn new(round: &str) -> Self {
         let mut round = round.split_whitespace();
+
+        let opponent = Shape::from_opponent(
+            get_char(round.next().unwrap())
+        );        
 
         let self_ = Shape::from_self(
             get_char(round.next().unwrap())
         );    
-        let opponent = Shape::from_opponent(
-            get_char(round.next().unwrap())
-        );
 
         assert!(round.next().is_none());
 
@@ -81,37 +78,56 @@ impl Round for SelfRound {
 
 }
 
-impl Round for ElfRound {
-    fn from_str(round: &str) -> Self {
-        ElfRound { self_: Shape::PAPER, outcome: Outcome::DRAW }
+impl ElfRound {
+    fn new(round: &str) -> Self {
+        let mut round = round.split_whitespace();
+
+        let opponent = Shape::from_opponent(
+            get_char(round.next().unwrap())
+        );    
+        let outcome = Outcome::new(
+            get_char(round.next().unwrap())
+        );
+
+        assert!(round.next().is_none());
+
+        Self{opponent, outcome}
     }
 
 }
 
+enum RoundType {
+    SELF,
+    ELF,
+}
 
-fn parse(input: &str, round_type: RoundType) -> impl Iterator<Item = SelfRound> + '_  {
+enum Round {
+    SELF(SelfRound),
+    ELF(ElfRound),
+}
+
+
+fn parse(input: &str, round_type: RoundType) -> impl Iterator<Item = Round> + '_ {
     input
         .lines()
         .map(move |round| {
-            let x = match round_type {
-                RoundType::SELF  => SelfRound::from_str(round),
-                // RoundType::ELF  => Box::new(ElfRound::from_str(round))
-                _ => panic!()
-            };
-            x
+            match round_type {
+                RoundType::SELF  => Round::SELF(SelfRound::new(round)),
+                RoundType::ELF  => Round::ELF(ElfRound::new(round)),
+            }
         })
 }
 
 
 fn part_one_round(round: &SelfRound) -> Outcome {
     match round {
-        SelfRound {self_, opponent} 
-        if self_ == opponent 
+        SelfRound {opponent, self_} 
+        if opponent == self_
             => Outcome::DRAW,
 
-        SelfRound {self_: Shape::ROCK, opponent: Shape::SCISSORS} 
-        | SelfRound {self_: Shape::SCISSORS, opponent: Shape::PAPER}
-        | SelfRound {self_: Shape::PAPER, opponent: Shape::ROCK} 
+        SelfRound {opponent: Shape::ROCK, self_: Shape::PAPER} 
+        | SelfRound {opponent: Shape::PAPER, self_: Shape::SCISSORS}
+        | SelfRound {opponent: Shape::SCISSORS, self_: Shape::ROCK} 
             => Outcome::WIN,
 
         _ 
@@ -120,12 +136,37 @@ fn part_one_round(round: &SelfRound) -> Outcome {
 }
 
 
+fn part_two_round(round: &ElfRound) -> Shape {
+    match round {
+        ElfRound {opponent, outcome: Outcome::DRAW} 
+            => *opponent,
+
+        ElfRound {opponent: Shape::PAPER, outcome: Outcome::LOSS} 
+        | ElfRound {opponent: Shape::SCISSORS, outcome: Outcome::WIN}
+            => Shape::ROCK,
+
+        ElfRound {opponent: Shape::ROCK, outcome: Outcome::WIN} 
+        | ElfRound {opponent: Shape::SCISSORS, outcome: Outcome::LOSS}
+            => Shape::PAPER,         
+
+        ElfRound {opponent: Shape::ROCK, outcome: Outcome::LOSS} 
+        | ElfRound {opponent: Shape::PAPER, outcome: Outcome::WIN}
+            => Shape::SCISSORS,    
+    }
+}
+
+
+
 pub fn part_one(input: &str) -> Option<u32> {
     let parsed = parse(input, RoundType::SELF);
     let summed_scores = parsed
         .map(
-            |round| 
+            |round_chaperone| 
             {
+                let round = match round_chaperone {
+                    Round::SELF(round) => round,
+                    _ => panic!("Unexpected round type")
+                };
                 let outcome_score = part_one_round(&round) as u32;
                 let self_score = round.self_ as u32;
                 outcome_score + self_score
@@ -136,7 +177,22 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let parsed = parse(input, RoundType::ELF);
+    let summed_scores = parsed
+        .map(
+            |round_chaperone| 
+            {
+                let round = match round_chaperone {
+                    Round::ELF(round) => round,
+                    _ => panic!("Unexpected round type")
+                };
+                let self_score = part_two_round(&round) as u32;
+                let outcome_score = round.outcome as u32;
+                outcome_score + self_score
+            }
+        )
+        .sum();
+    Some(summed_scores)
 }
 
 fn main() {
@@ -156,8 +212,47 @@ mod tests {
     }
 
     #[test]
+    fn test_part_one_individual_rounds() {
+        let input = [
+            ("A X", 4),
+            ("A Y", 8),
+            ("A Z", 3),
+            ("B X", 1),
+            ("B Y", 5),
+            ("B Z", 9),
+            ("C X", 7),
+            ("C Y", 2),
+            ("C Z", 6),
+        ];
+
+        for (round, score) in input {
+            assert_eq!(part_one(round), Some(score));
+        }
+    }
+
+    #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
+
+    #[test]
+    fn test_part_two_individual_rounds() {
+        let input = [
+            ("A X", 3),
+            ("A Y", 4),
+            ("A Z", 8),
+            ("B X", 1),
+            ("B Y", 5),
+            ("B Z", 9),
+            ("C X", 2),
+            ("C Y", 6),
+            ("C Z", 7),
+        ];
+
+        for (round, score) in input {
+            assert_eq!(part_two(round), Some(score));
+        }
+    }
+
 }
