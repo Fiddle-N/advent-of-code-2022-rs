da64d0fe35fa7d2a0f35c8c0d0bb9ea78d98ef3d
Fadil Nohur <fadil.nohur@outlook.com>
2023-04-09 00:21:29 +0100

    Day 5 - refactoring to add impl block

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,8 +1,7 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 
-
-enum Cratemover {
+enum CrateMover {
     CM9000,
     CM9001,
 }
@@ -13,53 +12,100 @@ struct Rearrangement {
     to: usize,
 }
 
-fn parse_stacks(stack_input: &str) -> Vec<Option<Vec<char>>> {
-    let mut rows = stack_input.lines().rev();
-    let header = rows.next().unwrap();
+struct SupplyStacks {
+    stacks: Vec<Option<Vec<char>>>
+}
 
-    let row_idxs: Vec<(usize, usize)> = header
-        .chars()
-        .enumerate()
-        .filter_map(
-            |idxs|
-            {
-                match idxs {
-                    (_, ' ') 
-                        => None,
+impl SupplyStacks {
 
-                    (idx, stack_idx) 
-                        => Some((idx, stack_idx.to_digit(10).unwrap() as usize))
-                }
-            }
-        )
-        .collect();
-
-    let rows = rows
-        .map(
-            |row|
-            row.chars().collect::<Vec<_>>()
-        );
-
-    let mut stacks = vec![None];    // initialise with None to make 1-based vec
-
-    for _ in &row_idxs {
-        stacks.push(Some(Vec::new()));
-    }
-
-    for row in rows {
-        for (idx, stack_idx) in &row_idxs {
-            let val = row[*idx];
-            if val == ' ' {
-                continue;
-            }
-
-            let stack = stacks.get_mut(*stack_idx).unwrap().as_mut().unwrap();
-
-            stack.push(val)
-        }
-    }
+    fn new(stack_input: &str) -> Self {
+        let mut rows = stack_input.lines().rev();
+        let header = rows.next().unwrap();
     
-    stacks
+        let row_idxs: Vec<(usize, usize)> = header
+            .chars()
+            .enumerate()
+            .filter_map(
+                |idxs|
+                {
+                    match idxs {
+                        (_, ' ') 
+                            => None,
+    
+                        (idx, stack_idx) 
+                            => Some((idx, stack_idx.to_digit(10).unwrap() as usize))
+                    }
+                }
+            )
+            .collect();
+    
+        let rows = rows
+            .map(
+                |row|
+                row.chars().collect::<Vec<_>>()
+            );
+    
+        let mut stacks = vec![None];    // initialise with None to make 1-based vec
+    
+        for _ in &row_idxs {
+            stacks.push(Some(Vec::new()));
+        }
+    
+        for row in rows {
+            for (idx, stack_idx) in &row_idxs {
+                let val = row[*idx];
+                if val == ' ' {
+                    continue;
+                }
+    
+                let stack = stacks.get_mut(*stack_idx).unwrap().as_mut().unwrap();
+    
+                stack.push(val)
+            }
+        }
+        
+        Self{stacks}
+    }
+
+    fn crate_mover(
+        &mut self, 
+        rearrangements: Vec<Rearrangement>,
+        cm: CrateMover
+    ) -> &mut Self {
+        for rearrangement in rearrangements{
+            let from_stack = self.stacks.get_mut(rearrangement.from).unwrap().as_mut().unwrap();
+    
+            let mut to_move = from_stack.split_off(from_stack.len() - rearrangement.move_);
+    
+            match cm {
+                CrateMover::CM9000 => to_move.reverse(),
+                CrateMover::CM9001 => (),
+            }
+    
+            let to_stack = self.stacks.get_mut(rearrangement.to).unwrap().as_mut().unwrap();
+    
+            to_stack.append(&mut to_move);
+    
+        }
+        self
+    }
+
+    fn top_stacks(&self) -> String {
+        let mut top_stacks = String::new();
+    
+        for stack in &self.stacks {
+            match stack {
+                None => (),
+                Some(stack) => {
+                    let last_val = *stack.last().unwrap();
+                    top_stacks.push(last_val);
+                },
+            }
+        }
+    
+        top_stacks
+    }
+
 }
 
 fn parse_rearrangements(rearrangement_input: &str) -> Vec<Rearrangement> {
@@ -85,78 +131,31 @@ fn parse_rearrangements(rearrangement_input: &str) -> Vec<Rearrangement> {
         .collect()
 }
 
-
-fn cratemover(
-    mut stacks: Vec<Option<Vec<char>>>, 
-    rearrangements: Vec<Rearrangement>,
-    cm: Cratemover
-) -> Vec<Option<Vec<char>>> {
-    for rearrangement in rearrangements{
-        let from_stack = stacks.get_mut(rearrangement.from).unwrap().as_mut().unwrap();
-
-        let mut to_move = from_stack.split_off(from_stack.len() - rearrangement.move_);
-
-        match cm {
-            Cratemover::CM9000 => to_move.reverse(),
-            Cratemover::CM9001 => (),
-        }
-
-        let to_stack = stacks.get_mut(rearrangement.to).unwrap().as_mut().unwrap();
-
-        to_stack.append(&mut to_move);
-
-    }
-    stacks
-}
-
-fn top_stacks(stacks: Vec<Option<Vec<char>>>) -> String {
-    let mut top_stacks = String::new();
-
-    for stack in stacks {
-        match stack {
-            None => (),
-            Some(stack) => {
-                let last_val = *stack.last().unwrap();
-                top_stacks.push(last_val);
-            },
-        }
-    }
-
-    top_stacks
-}
-
-fn supply_stacks(input: &str, cm: Cratemover) -> String {
+fn supply_stacks(input: &str, cm: CrateMover) -> String {
     let mut input = input.split("\n\n");
 
-    let stacks = input.next().unwrap();
-    let mut stacks = parse_stacks(stacks);
+    let stack_input = input.next().unwrap();
+    let mut stacks = SupplyStacks::new(stack_input);
 
-    let rearrangements = input.next().unwrap();
-    let rearrangements = parse_rearrangements(rearrangements);
+    let rearrangement_input = input.next().unwrap();
+    let rearrangements = parse_rearrangements(rearrangement_input);
 
     assert!(input.next().is_none());
 
-    match cm {
-        Cratemover::CM9000 => {
-            stacks = cratemover(stacks, rearrangements, cm);
-        },
-        Cratemover::CM9001 => {
-            stacks = cratemover(stacks, rearrangements, cm);
-        },
-    }
+    stacks.crate_mover(rearrangements, cm);
     
-    let top_stacks = top_stacks(stacks);
+    let top_stacks = stacks.top_stacks();
     
     top_stacks
 }
 
 pub fn part_one(input: &str) -> Option<String> {
-    let result = supply_stacks(input, Cratemover::CM9000);
+    let result = supply_stacks(input, CrateMover::CM9000);
     Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<String> {
-    let result = supply_stacks(input, Cratemover::CM9001);
+    let result = supply_stacks(input, CrateMover::CM9001);
     Some(result)
 }
 
