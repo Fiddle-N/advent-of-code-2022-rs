fa43e83fa2144cfb3cac324cc257fe35c7ff46f2
Fadil Nohur <fadil.nohur@outlook.com>
2023-04-15 19:59:32 +0100

    Day 7 Part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,24 +1,22 @@
 use std::{cell::RefCell, collections::HashMap, rc::{Weak, Rc}};
 
-use itertools::Itertools;
-
 #[derive(Debug)]
 struct File {
     size: u64,
 }
 
 #[derive(Debug)]
-struct Dir_ {
+struct DirInner {
     subdirs: HashMap<String, Dir>,
     files: HashMap<String, File>,
     parent: WeakDir,
 }
 
 #[derive(Debug, Clone)]
-struct Dir(Rc<RefCell<Dir_>>);
+struct Dir(Rc<RefCell<DirInner>>);
 
 #[derive(Debug, Clone)]
-struct WeakDir(Weak<RefCell<Dir_>>);
+struct WeakDir(Weak<RefCell<DirInner>>);
 
 
 impl Dir {
@@ -32,7 +30,7 @@ impl Dir {
             None => WeakDir(Weak::new()),
         };
 
-        Self(Rc::new(RefCell::new(Dir_ {
+        Self(Rc::new(RefCell::new(DirInner {
             subdirs: HashMap::new(), 
             files: HashMap::new(),
             parent: weak_parent,
@@ -50,24 +48,19 @@ impl Dir {
     }
 
     fn cd(&self, dir_name: &str) -> Dir {
-        let mut dir = self.0.borrow_mut();
+        let dir = self.0.borrow();
 
         match dir_name {
             ".." => {
-                let parent = &dir.parent;
-                let parent = parent.clone();
-                let parent = parent.0.upgrade().unwrap();
+                let weak_parent = dir.parent.clone();
+                let parent = weak_parent.0.upgrade().unwrap();
 
                 Dir(parent)
             }
             _ => {
                 let subdirs = &dir.subdirs;
-                let result = subdirs.get(dir_name);
-
-                let result2 = result.unwrap();
-
-                let result3 = result2.clone();      // performs underlying Rc clone
-                result3
+                let subdir = subdirs.get(dir_name).unwrap().clone();
+                subdir
             }
         }
     }
@@ -100,31 +93,7 @@ impl Dir {
 
 }
 
-fn dir_search(fs: Dir) -> Vec<Dir> {
-    let mut dirs: Vec<Dir> = Vec::new();
-    let max_size = 100000;
-
-    fn search(dir_: Dir, max_size: u64, dirs: &mut Vec<Dir>)
-        {
-            let dir_clone = dir_.clone();
-            if dir_.size() < max_size {
-                dirs.push(dir_clone)
-            }
-
-            let dir_clone = dir_.clone();
-            let dir_2 = dir_clone.0.borrow();
-            for dir_3 in dir_2.subdirs.values() {
-                let dir_clone = dir_3.clone();
-                search(dir_clone, max_size, dirs)
-            }
-        };
-
-    search(fs, max_size, &mut dirs);
-    dirs
-
-}
-
-pub fn part_one(input: &str) -> Option<u64> {
+fn create_fs(input: &str) -> Dir {
     let mut input = input.lines();
 
     let fs;
@@ -168,7 +137,47 @@ pub fn part_one(input: &str) -> Option<u64> {
         }
     }
 
-    let dirs_under_100k = dir_search(fs);
+    fs
+}
+
+#[derive(Copy, Clone)]
+enum DirSizeComps {
+    LE,
+    GE,
+}
+
+fn dir_search(fs: Dir, size: u64, comp: DirSizeComps) -> Vec<Dir> {
+    let mut dirs: Vec<Dir> = Vec::new();
+
+    fn search(dir_: Dir, dirs: &mut Vec<Dir>, size: u64, comp: DirSizeComps)
+    {
+        let dir_clone = dir_.clone();
+        let comp_result = match comp {
+            DirSizeComps::LE => dir_.size() <= size,
+            DirSizeComps::GE => dir_.size() >= size,
+        };
+
+        if comp_result {
+            dirs.push(dir_clone)
+        }
+
+        let dir_clone = dir_.clone();
+        let dir_inner = dir_clone.0.borrow();
+        for subdir in dir_inner.subdirs.values() {
+            let subdir_clone = subdir.clone();
+            search(subdir_clone, dirs, size, comp)
+        }
+    }
+
+    search(fs, &mut dirs, size, comp);
+    dirs
+
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let fs = create_fs(input);
+
+    let dirs_under_100k = dir_search(fs, 100_000, DirSizeComps::LE);
 
     let result: u64 = dirs_under_100k
         .iter()
@@ -178,11 +187,25 @@ pub fn part_one(input: &str) -> Option<u64> {
         .sum();
 
     Some(result)
-
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let fs = create_fs(input);
+
+    let size_left = 70_000_000 - fs.size();
+    let size_req = 30_000_000 - size_left;
+
+    let dirs_to_free_space = dir_search(fs, size_req, DirSizeComps::GE);
+
+    let result: u64 = dirs_to_free_space
+        .iter()
+        .map(
+            |dir| dir.size()
+        )
+        .min()
+        .unwrap();
+
+    Some(result)
 }
 
 fn main() {
@@ -204,6 +227,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 7);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(24933642));
     }
 }
